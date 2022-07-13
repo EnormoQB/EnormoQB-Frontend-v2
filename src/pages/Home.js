3481e9f9d7fe267d99edd390d31f152f58e72822
apoorva-s1301 <apoorvasingh130@gmail.com>
2022-07-13 22:10:24 +0530

    Add process block(WIP)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import NavBar from '../components/Navbar';
 import TopBorder from '../components/TopBorder';
 import LandingIllustration from '../assets/landing.svg';
 import SampleDashboard from '../assets/sample.png';
+// import LoginIllustration from '../assets/login.svg';
 import '../styles/Home/index.scss';
 
 const Hero = () => (
@@ -24,12 +25,41 @@ const Hero = () => (
   </Flex>
 );
 
+// const ProcessBlock = ({ flexD, imageSrc, animatedHead, nonAnimatedHead, content }) => (
+//   <Flex flexDirection='column' alignItems='center' color='brand.600'>
+//     <Flex mt='14' w='80%' flexDirection={flexD}>
+//       <Image src={imageSrc} alt='Login Illustration' w='50%' p='10' />
+//       <Flex flexDirection='column' justifyContent='center' w='50%' p='10'>
+//         <Box fontSize='2xl' fontWeight='bold'>
+//           <span className='highlight'>{animatedHead}</span>
+//           {nonAnimatedHead}
+//         </Box>
+//         <Box fontSize='lg' mt='8'>
+//           {content}
+//         </Box>
+//       </Flex>
+//     </Flex>
+//   </Flex>
+// );
+
 const Home = () => (
   <Flex flexDirection='column' bg='brand.100' h='100%'>
     <TopBorder />
     <NavBar />
     <Hero />
     <Image mx='10%' src={SampleDashboard} alt='Sample Dashboard' />
+    <Flex flexDirection='column' alignItems='center' color='brand.600'>
+      {/* <ProcessBlock
+        flexD='row'
+        imageSrc={LoginIllustration}
+        animatedHead='Sign in'
+        nonAnimatedHead='to your free account'
+        content='All you need is your gmail address to create an account/sign in.Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.'
+      /> */}
+    </Flex>
   </Flex>
 );
 
