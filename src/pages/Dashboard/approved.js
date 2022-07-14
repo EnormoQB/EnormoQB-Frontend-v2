4dba80b5415f1a72a48ca3f91a3fdfb3a167e92c
Prakharsharma2206 <prakharsharma22@gmail.com>
2022-07-15 01:50:17 +0530

    Accept and Reject

--- src/pages/Dashboard/approved.js
+++ src/pages/Dashboard/approved.js
@@ -1,5 +1,29 @@
+import { Box } from '@chakra-ui/react';
+import AQuestions from '../../components/Accepted/AQuestions';
+import { Value } from '../../components/Accepted/config';
+
 const Approved = () => {
-  return <div>Approved</div>;
+  return (
+    <Box>
+      {Value.map((value) => (
+        <AQuestions
+          key={value.id}
+          question={value.question}
+          option1={value.option1}
+          option2={value.option2}
+          option3={value.option3}
+          option4={value.option4}
+          answer={value.answer}
+          standard={value.standard}
+          subject={value.subject}
+          chapter={value.chapter}
+          difficulty={value.difficulty}
+          imageurl={value.imageurl}
+          alt={value.alt}
+        />
+      ))}
+    </Box>
+  );
 };
 
 export default Approved;
