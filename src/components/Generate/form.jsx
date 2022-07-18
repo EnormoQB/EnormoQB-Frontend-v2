3e18ddf3b959cd33b03ee6c647b2bd77cede78ad
ankitgoel25 <ankitoct01@gmail.com>
2022-07-19 00:39:38 +0530

    Contribute form completed, dropzone added

--- src/components/Generate/form.jsx
+++ src/components/Generate/form.jsx
@@ -18,6 +18,7 @@ const GenerateForm = () => {
       setSubjects(class12);
     }
   };
+
   return (
     <Box>
       {/* Class */}
@@ -50,10 +51,9 @@ const GenerateForm = () => {
           border='gray.200'
           borderWidth={1}
           onChange={(e) => setSubject(e.target.value)}
-          // color='gray.400'
         >
-          {subjects.map((value, i) => (
-            <option key={i}>{value}</option>
+          {subjects.map((value) => (
+            <option key={value}>{value}</option>
           ))}
         </Select>
       </FormControl>
