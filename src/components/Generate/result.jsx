43046712aa6554dfe4be1702d9c4968d9daa834a
apoorva-s1301 <apoorvasingh130@gmail.com>
2022-07-21 03:38:52 +0530

    Add drag and drop functionality to generate ques paper.

--- src/components/Generate/result.jsx
+++ src/components/Generate/result.jsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import CustomQuestion from './customQues';
 import QuestionTab from './questionTab';
 import { dummy } from './config';
 
 const GenerateResult = () => {
+  const [initialData, setInitialData] = useState(dummy);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+    const items = Array.from(initialData);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setInitialData(items);
+  }
+
   return (
-    <div>
-      <CustomQuestion />
-      {dummy.map((ques, idx) => (
-        // eslint-disable-next-line no-underscore-dangle
-        <QuestionTab key={ques._id.$oid} index={idx + 1} data={ques} />
-      ))}
-    </div>
+    // eslint-disable-next-line react/jsx-no-bind
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId='list'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <CustomQuestion />
+            {initialData.map((ques, idx) => (
+              // eslint-disable-next-line no-underscore-dangle
+              <QuestionTab key={ques._id.$oid} index={idx} data={ques} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
