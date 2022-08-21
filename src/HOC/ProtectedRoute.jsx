c3f92d18fba21f5a3365d43dd7628d7979cb13b2
Apoorv Dwivedi <apoorvd14@gmail.com>
2022-08-21 16:04:42 +0530

    authorized routes

--- src/HOC/ProtectedRoute.jsx
+++ src/HOC/ProtectedRoute.jsx
@@ -3,6 +3,37 @@ import { useSelector } from 'react-redux';
 import { useGetUserDataQuery } from '../redux/services/userApi';
 import FullScreenLoader from '../components/Loaders/FullScreenLoader';
 
+const allowedRoutes = {
+  admin: [
+    '/dashboard',
+    '/dashboard/pending',
+    '/dashboard/contribute',
+    '/dashboard/generate',
+    '/dashboard/questionpapers',
+    '/dashboard/requestContributions',
+  ],
+  member: [
+    '/dashboard',
+    '/dashboard/pending',
+    '/dashboard/approved',
+    '/dashboard/rejected',
+    '/dashboard/contribute',
+    '/dashboard/questionpapers',
+    '/dashboard/perks',
+  ],
+  developer: [
+    '/dashboard',
+    '/dashboard/pending',
+    '/dashboard/approved',
+    '/dashboard/rejected',
+    '/dashboard/contribute',
+    '/dashboard/generate',
+    '/dashboard/questionpapers',
+    '/dashboard/perks',
+    '/dashboard/requestContributions',
+  ],
+};
+
 const ProtectedRoute = ({ children }) => {
   const location = useLocation();
   const user = useSelector((state) => state.userState.user);
@@ -12,7 +43,8 @@ const ProtectedRoute = ({ children }) => {
     return <FullScreenLoader />;
   }
 
-  return user ? (
+  return user &&
+    allowedRoutes[user.userType.toLowerCase()].includes(location.pathname) ? (
     children
   ) : (
     <Navigate to='/' state={{ from: location }} replace />
