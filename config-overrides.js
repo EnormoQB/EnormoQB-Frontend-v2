e149677745f9257e55342791df3eae798b8d7c8f
Apoorv Dwivedi <apoorvd14@gmail.com>
2022-08-26 14:33:57 +0530

    pollyfill issues resolved

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+  plugins: {
+    add: [
+      new NodePolyfillPlugin({
+        excludeAliases: ['console'],
+      }),
+    ],
+  },
+  resolve: {
+    modules: ['node_modules'],
+  },
+  node: {
+    fs: 'empty',
+  },
+};
